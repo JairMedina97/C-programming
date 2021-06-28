5839919951a6de4e4b6e50ca099940be4cfd3030
Jair Medina <54874269+JairMedina97@users.noreply.github.com>
2021-06-27 21:46:44 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,85 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double cube(double num){
-    double result = num*num*num;
-    return result;
-}
-
+struct Student{
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+};
 
 int main()
 {
-    printf("Hello\nWorld\n");
-    printf("Hello\"World\n");
-    char characterName[] = "Tom";
-    int characterAge = 67;
-    printf("There once was a man named %s\n", characterName);
-    printf("He was %d years old.\n", characterAge);
-
-    characterAge = 30;
-    printf("He really liked the name %s\n", characterName);
-    printf("but did not like being %d.\n", characterAge);
-
-/* Data Types */
-
-    int age = 40;
-    double gpa = 3.7;
-    char grade = 'A';
-    char phrase[] = "Giraffe Academy";
-
-/* Format specifier */
-    double favNum = 500.98754;
-    printf("My favorite %s is %f \n","number" , favNum);
-
-/* Working with numbers */
-
-    printf("%f \n", 5 / 4.0);
-    printf("%f \n", pow(2, 3));    /* 2 power 3*/
-    printf("%f \n", sqrt(36));     /* square root 36*/
-    printf("%f \n", ceil(36.35));  /* round this number up 36*/
-    printf("%f \n", floor(36.35)); /* floor this number up 36*/
-
-/* Constants */
-//: This is used to write a single line comment
-    const int NUM = 5;  /* Cant be modified, error*/
-    printf("%d\n", NUM);
-
-
-/* Basic Calculator */
     double num1;
     double num2;
-    printf("Enter first number: \n");
+    char op;
+
+    printf("Enter a number: ");
     scanf("%lf", &num1);
-    printf("Enter second number: \n");
+    printf("Enter operator: ");
+    scanf(" %c", &op);
+    printf("Enter a number: ");
     scanf("%lf", &num2);
-    printf("Answer: %f \n", num1+ num2);
 
-/* Arrays */
+    if( op == '+'){
+        printf("%f \n", num1 + num2);
+    } else if( op == '-'){
+        printf("%f \n", num1 - num2);
+    } else if( op == '*'){
+        printf("%f \n", num1 * num2);
+    } else if( op == '/'){
+        printf("%f \n", num1 / num2);
+    } else {
+        printf("Invalid Operator \n");
+    }
+
+    /* SWITCH Statements   */
+
+    char grade = 'A';
+
+    switch(grade){
+    case 'A' :
+        printf("You did great! \n");
+        break;
+    case 'B' :
+        printf("You did alright! \n");
+        break;
+    case 'C' :
+        printf("You did poorly! \n");
+        break;
+    case 'D' :
+        printf("You did very bad! \n");
+        break;
+    default :
+        printf("Invalid Grade \n");
+    }
+
+/*Structs to store int, double, strings in one single Data structure  */
+
+    struct Student student1;
+    student1.age = 22;
+    student1.gpa = 3.2;
+    strcpy( student1.name, "Jim");
+    strcpy( student1.major, "Business");
+/*
+    struct Student student2;
+    student2.age = 20;
+    student2.gpa = 2.5;
+    strcpy( student2.name, "Pam");
+    strcpy( student2.major, "Art"); */
+
+    printf("%s \n", student1.name);
+
+ /* While loops */
+    int i = 1;
+    while(i <= 5){
+        printf("%d\n", i);
+        i++;
+ /*     i = i + 1; */
+    }
+
+ /* For loops */
+    int j;
+    for(j = 1; j <=5; j++){
+        printf("%d\n", j);
+    }
+
     int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
-    int tenitems[10];
-    tenitems[1] = 80;
-    tenitems[0] = 90;
-    luckyNumbers[1] = 200;
-    printf("%d \n", luckyNumbers[1]);
-    printf("%d \n", tenitems[1]);
-
-
-    sayHi("Mike", 40);
-    sayHi("Tom", 23);
-    sayHi("Oscar", 70);
-
-    /* Return function cube */
-    //:cube(double num)
-    printf("Answer of cubic function: %f \n", cube(4.0));
-
-
+    int k;
+    for(k = 0; k <6; k++){
+        printf("%d\n", luckyNumbers[k]);
+    }
 
     return 0;
 }
-
-void sayHi(char name[], int age){
-    printf("Hello %s you are %d \n", name, age);
-}
-
-
-
